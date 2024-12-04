33a7140bc64ba072bce774342a8d4bad9ceadb03
srho1212 <si990009@yahoo.co.jp>
2024-12-04 23:23:25 +0900

    細部修正

--- app/controllers/purchase_informations_controller.rb
+++ app/controllers/purchase_informations_controller.rb
@@ -1,13 +1,13 @@
 class PurchaseInformationsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!  # すべてのアクションで認証を要求
   before_action :set_item, only: [:index, :create]
-
+  before_action :deny_access_if_sold_or_owner, only: [:index, :create]
+  
   def index
-    #@item = Item.find(params[:item_id])
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_informationsform = PurchaseInformationsForm.new
   end
-
+  
   def create
     @purchase_informationsform = PurchaseInformationsForm.new(purchase_informations_params)
     if @purchase_informationsform.valid?
@@ -15,30 +15,35 @@ class PurchaseInformationsController < ApplicationController
       @purchase_informationsform.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
-
+  
   private
-
+  
   def set_item
     @item = Item.find(params[:item_id])
   end
-
+  
+  def deny_access_if_sold_or_owner
+    if @item.purchase_information.present? || @item.user_id == current_user.id
+      redirect_to root_path, alert: 'この商品は購入できません。'
+    end
+  end
+  
   def purchase_informations_params
     params.require(:purchase_informations_form).permit(
-      :post_code, :shipping_prefecture_id, :municipality, :street_address, :building_name, :telephone).merge(item_id: params[:item_id], user_id: current_user.id,token: params[:token])
+      :post_code, :shipping_prefecture_id, :municipality, :street_address, :building_name, :telephone
+    ).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
-
+  
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: purchase_informations_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price,
+      card: purchase_informations_params[:token],
+      currency: 'jpy'
     )
   end
-
 end
