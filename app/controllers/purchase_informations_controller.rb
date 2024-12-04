e314ee70a2765b61e014aaaadf7fe5075ae5706f
srho1212 <si990009@yahoo.co.jp>
2024-12-04 17:52:36 +0900

    クレジットカード決済（クレジットカード情報トークン化）

--- app/controllers/purchase_informations_controller.rb
+++ app/controllers/purchase_informations_controller.rb
@@ -10,6 +10,7 @@ class PurchaseInformationsController < ApplicationController
   def create
     @purchase_informationsform = PurchaseInformationsForm.new(purchase_informations_params)
     if @purchase_informationsform.valid?
+      #pay_item # トークンを使った支払い処理を呼び出す
       @purchase_informationsform.save
       redirect_to root_path
     else
@@ -29,4 +30,13 @@ class PurchaseInformationsController < ApplicationController
       :post_code, :shipping_prefecture_id, :municipality, :street_address, :building_name, :telephone
     ).merge(item_id: params[:item_id], user_id: current_user.id)
   end
+
+ #def pay_item
+  #Payjp.api_key = ENV['sk_test_ee29726f757bb33bd9c5f5ea'] # PAY.JP秘密鍵
+  #Payjp::Charge.create(
+    #amount: @item.price, # 金額
+    #card: purchase_informations_params[:token], # カードトークン
+    #currency: 'jpy'
+  #)
+ #end
 end
